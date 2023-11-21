e54dac969d8223cd039d18c5ab149a2c6390f3e4
MarcoPicca <il96marco@gmail.com>
2023-11-21 17:10:42 +0100

    js campominato grid quasi completo

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella,
+//  la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+
+
+let buttonGame = document.getElementById('button');
+
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+
+
+
+buttonGame.addEventListener('click', function () {
+    function getNewSquare(){
+        const newSquareElement = document.createElement('article');
+        newSquareElement.classList.add('item-square');
+        return newSquareElement;
+    }
+    
+    
+    const mainContentEl = document.querySelector('main div.main-content');
+    
+    for (let i = 1; i <= 100; i++){
+        const currentSquare = getNewSquare();
+
+
+        // Ogni cella ha un numero progressivo, da 1 a 100.
+
+        const squareContent = i
+    
+        currentSquare.innerHTML += `<span> ${squareContent} </span>`;
+    
+        currentSquare.addEventListener('click', function (){
+            currentSquare.classList.toggle('clicked');
+            console.log(squareContent);
+        });
+        mainContentEl.appendChild(currentSquare);
+    }
+    
+    function getRandomNumber (minNumber, maxNumber) {
+        return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    }
+
+});
+
